628e2fbfa6a737201b20e74081718604d0afdb53
Аркадий Ежов <prikol93745774@gmail.com>
2026-02-25 16:19:52 +0300

    Добавьте файлы проекта.

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        List<User> users = new List<User>()
+            {
+            new User()
+        {
+            Id=1,
+            FullName="Петров Иван Иванович",
+            BirthDate=new DateOnly(1988,2,5)     
+        },
+         new User()
+        {
+            Id = 2,
+            FullName = "Петр%ов Егор Алексеевич",
+            BirthDate = new DateOnly(1994, 12, 11)
+
+
+        },
+        new User()
+        {
+            Id = 3,
+            FullName = "Егорова Дарья Евге!ньевна",
+            BirthDate = new DateOnly(2005, 3, 1)
+
+
+        }
+    };
+
+        [HttpGet(Name = "GetUsers")]
+
+        public IEnumerable<User> Get()
+        {
+            return users;
+        }
+    }
+}
---
+++ Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
